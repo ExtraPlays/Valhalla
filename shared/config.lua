2188004dd89a502162f0ff736f9bc563f65f4b91
ExtraPlays <extraplays324@gmail.com>
2024-12-18 06:41:50 -0300

    :recycle: refactor: Atualização do objeto de skin

--- shared/config.lua
+++ shared/config.lua
@@ -4,7 +4,16 @@ Config = {}
 Config.Maintenance = false
 
 --- Default skin for new players
-Config.DefaultSkin = { model = 'mp_m_freemode_01', drawables = {}, textures = {} }
+Config.DefaultSkin = {
+  model = 'mp_f_freemode_01',
+  components = {
+    ["0"] = { drawable = 1, texture = 0 },  -- Máscara
+    ["1"] = { drawable = 5, texture = 0 },  -- Cabelo
+    ["3"] = { drawable = 15, texture = 0 }, -- Mãos
+    ["4"] = { drawable = 10, texture = 0 }, -- Calça
+    ["6"] = { drawable = 5, texture = 0 }   -- Sapatos
+  }
+}
 
 
 return Config
